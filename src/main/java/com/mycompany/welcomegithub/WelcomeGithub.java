3a11a81a6fa89fddb25a4212570c2ccc21799347
viet81918 <viet81918@gmail.com>
2024-05-15 16:21:51 +0700

    Finish Project

---
+++ src/main/java/com/mycompany/welcomegithub/WelcomeGithub.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.welcomegithub;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class WelcomeGithub {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
